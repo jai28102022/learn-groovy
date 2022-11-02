8a4d9dfb9efc42c7dc3efb07bc1a72181ae0d859
jai28102022 <116903411+jai28102022@users.noreply.github.com>
2022-11-02 11:41:10 +0530

    Create string_op.groovy

---
+++ string_op.groovy
@@ -0,0 +1,8 @@
+String a1 = "Hello World";
+println(a1[0]);
+println(a1.toLowerCase());
+println(a1.toUpperCase());
+println(a1.reverse());
+
+String log = "2022-10-10 12:11:00 404"
+println(log[0..10]);
