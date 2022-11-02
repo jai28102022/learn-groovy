d29f37e9f7876ac94bf063f5c46285fc1cc0546f
jai28102022 <116903411+jai28102022@users.noreply.github.com>
2022-11-02 10:33:33 +0530

    Create loops.groovy

---
+++ loops.groovy
@@ -0,0 +1,15 @@
+int num = 1;
+
+while (num <= 10) {
+  print(num + " ");
+  num++;
+}
+
+for(int num2=10; num2 <= 20; num2++) {
+  print(num2 + " ");
+}
+
+int[] iarr = [1,2,3,4,5];
+for(int x1 in iarr) {
+    print(x1 + " ");
+}
