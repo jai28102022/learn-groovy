624972653685e0f6f1eb0cb71b98511642fce4b8
jai28102022 <116903411+jai28102022@users.noreply.github.com>
2022-11-02 11:57:31 +0530

    Create ranges.groovy

---
+++ ranges.groovy
@@ -0,0 +1,21 @@
+def range01 = 1..10;
+for(x1 in range01){
+    print(x1 + " ");
+}
+
+
+def range02 = 'a'..'z'
+for(x1 in range02){
+    print(x1 + " ");
+}
+
+
+def range03 = 10..1;
+for(x1 in range03){
+    print(x1 + " ");
+}
+
+def range04 = 'z'..'a'
+for(x1 in range04){
+    print(x1 + " ");
+}
