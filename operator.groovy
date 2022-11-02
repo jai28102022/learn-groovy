cd096c59201e7de53ae247059440cab0af60b60b
jai28102022 <116903411+jai28102022@users.noreply.github.com>
2022-11-02 09:54:51 +0530

    Create operator.groovy

---
+++ operator.groovy
@@ -0,0 +1,27 @@
+int x1 = 10;
+int y1 = 30;
+
+println("Sum: " + (x1 + y1));
+println("Subtracts: " + (y1 - x1));
+println("Multiplication: " + (x1 * y1));
+println("Division: " + (y1 / x1));
+
+int x2 = 3;
+int y2 = 2;
+println("Remainder: " + (x2%y2));
+
+
+println("y2++: " + y2++);
+println("y2: " + y2);
+
+println("++y2: " + ++y2);
+println("y2: " + y2);
+
+
+println("y2--: " + y2--);
+println("y2: " + y2);
+
+println("--y2: " + --y2);
+println("y2: " + y2);
+
+
